198cdd1b75a8353aca1924bfb23fbb1ee88ed74e
Oskardmt009 <oskardavidgamboavalencia90@gmail.com>
2025-08-15 23:02:25 -0500

    Avance actividad

--- EjercicioCondicionales/condicionales.js
+++ EjercicioCondicionales/condicionales.js
@@ -80,3 +80,119 @@ else
 {
     console.log("No hay suma")
 }
+
+//En un almacén se hace un 20% de descuento a los clientes cuya 
+// compra sea superior a $100.000. ¿Cuánto deberá pagar cada cliente?
+
+let compra = 100000;
+const porc1 = 20;
+let dcto1 = porc1/100
+
+if(compra>=100000)
+{
+    vlrDcto1 = compra-(compra*dcto1);
+    console.log("El valor a pagar es: " + vlrDcto1)
+}
+else
+{
+    console.log("El valor a pagar es: " + compra)
+}
+
+//Calcular el salario de un empleado, teniendo en cuenta que si el salario bruto 
+// es superior a $2.000.000, debe hacerse una retención del 10%.
+
+let salario = 2000000;
+const porc2 = 10;
+let dcto2 = porc2/100;
+
+if(salario>2000000)
+{
+    salarioTotal = salario-(salario*dcto2);
+    console.log("El salario a pagar es: " + salarioTotal);
+}
+else
+{
+    console.log("El salario a pagar es: " + salario);
+}
+
+//Leer un número e imprimir un mensaje en caso que sea múltiplo de 5.
+
+let num3 = 15;
+if(num3 % 5 === 0)
+{
+    console.log("El número " + num3 + "Es múltiplo de 5")
+}
+else
+{
+    console.log("El número " + num3 + "No es múltiplo de 5")
+}
+
+//Leer la edad de una persona e imprimir un mensaje en caso que sea mayor de edad.
+
+let edad = 25;
+if(edad>=18)
+{
+    console.log("Mayor de Edad");
+}
+else
+{
+    (console.log)("Menor de Edad")
+}
+
+//Un proveedor de computadores ofrece descuento del 10%, si cuesta $1.000.000 
+// o más. Determinar cuánto pagará, con IVA incluido (19%), un cliente cualquiera por la compra de una computadora.
+
+if(costo >=1000000)
+{
+    vlrTotal= (costo*dcto)+costo;
+    costoIva = vlrTotal*ivaInc;
+    vlrPagar = vlrTotal+costoIva;
+
+    console.log("El valor a pagar del producto es: "+vlrPagar)
+}
+else
+{
+    vlrPagar=(costo*ivaInc)+costo
+    console.log("El valor a pagar del producto es: "+vlrPagar)
+}
+
+//Hacer un algoritmo que imprima el nombre de un artículo, clave, precio original 
+// y su precio con descuento. El descuento lo hace en base a la clave, si la 
+// clave es 01 el descuento es del 10% y si la clave es 02 el descuento es del 20% 
+// (solo existen dos claves).
+
+let articulo = "camisa";
+let clave = 1;
+let precioOriginal = 150000;
+let precioDcto = 0;
+
+if(clave ==1)
+{
+    precioDcto=precioOriginal*0.9;
+    console.log("El artículo "+articulo+" con la clave"+clave+"tiene un valor de: "+precioOriginal+ "y con descuento queda en: "+precioDcto)
+}
+else if(clave==2)
+{
+    precioDcto=precioOriginal*0.8;
+    console.log("El artículo "+articulo+" con la clave"+clave+"tiene un valor de: "+precioOriginal+ "y con descuento queda en: "+precioDcto)
+
+}
+else
+{
+    console.log("El artículo "+articulo+"tiene un valor de: "+precioOriginal)
+}
+
+//Hacer un algoritmo que calcule el total a pagar por la compra de camisas. 
+// Si se compran tres camisas o más se aplica un descuento del 20% sobre el 
+// total de la compra y si son menos de tres camisas un descuento del 10%
+
+let numCamisas = 3;
+let costoTotalCamisas = 1450000 //Suponiendo la suma total de las camisetas
+if(numCamisas<3)
+{
+    console.log("El valor total a pagar es de: "+ costoTotalCamisas*0.90)
+}
+else
+{
+    console.log("El valor total a pagar es de: "+ costoTotalCamisas*0.80)
+}
